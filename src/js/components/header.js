7ad0be3c0104ac4d5a587a8ca25b0b144391074e
Volodymyr <vladikon70@gmail.com>
2023-08-12 22:07:02 +0200

    update files

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,20 +1,11 @@
 import refs from '../refs';
-const {
-  burgerBtn,
-  iconClose,
-  iconBurger,
-  mobileMenu,
-  shoppingList,
-  homeLink,
-  sliderUkraine,
-} = refs;
+const { burgerBtn, iconClose, iconBurger, mobileMenu } = refs;
 ///////////////////////////Перемикач кнопки бургер//////////////////////////////
 let isOpen = true;
 burgerBtn.addEventListener('click', handlerChangeBtnMenu);
 function handlerChangeBtnMenu() {
   const refs = {
     blockAuth: document.querySelector('.block-auth'),
-    categoryChoice: document.querySelector('.category-choice'),
   };
   if (isOpen) {
     iconClose.classList.remove('is-hidden');
@@ -23,8 +14,6 @@ function handlerChangeBtnMenu() {
     setTimeout(() => {
       refs.blockAuth.style.display = 'block';
     }, 250);
-    // sliderUkraine.style.zIndex = -1;
-    refs.categoryChoice.classList.add('is-hidden');
     document.body.classList.add('pos-fixed');
     isOpen = false;
   } else {
@@ -32,8 +21,6 @@ function handlerChangeBtnMenu() {
     iconBurger.classList.remove('is-hidden');
     mobileMenu.classList.add('is-hidden');
     refs.blockAuth.style.display = 'none';
-    // sliderUkraine.style.zIndex = 1;
-    refs.categoryChoice.classList.remove('is-hidden');
     document.body.classList.remove('pos-fixed');
     isOpen = true;
   }
